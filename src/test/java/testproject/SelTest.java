9e63378b6e0c6cc004f1767b9869ed1dc457961f
ajamit24 <37693051+ajamit24@users.noreply.github.com>
2020-06-07 16:55:27 +0530

    Update SelTest.java

--- src/test/java/testproject/SelTest.java
+++ src/test/java/testproject/SelTest.java
@@ -8,7 +8,7 @@ import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-//File updated on 7th June - URL updated for PHP site -1
+//File updated on 7th June - URL updated for PHP site
 public class SelTest {
 	
 	WebDriver driver;
