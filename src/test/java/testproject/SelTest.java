f95c6a5cc78b0d35ee396edaae9df19dc0341dee
ajamit24 <37693051+ajamit24@users.noreply.github.com>
2020-06-07 14:11:11 +0530

    Update SelTest.java

--- src/test/java/testproject/SelTest.java
+++ src/test/java/testproject/SelTest.java
@@ -8,7 +8,7 @@ import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-//File updated on 7th June - URl updated for PHP site according to puppet agent
+//File updated on 7th June - URl updated for PHP site according to puppet agent machine
 public class SelTest {
 	
 	WebDriver driver;
