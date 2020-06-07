06590531d37422e8696c6bd341ea38b1582988be
ajamit24 <37693051+ajamit24@users.noreply.github.com>
2020-06-07 12:10:02 +0530

    Update SelTest.java

--- src/test/java/testproject/SelTest.java
+++ src/test/java/testproject/SelTest.java
@@ -8,7 +8,7 @@ import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-//File updated on 7th June for pipeline
+//File updated on 7th June
 public class SelTest {
 	
 	WebDriver driver;
