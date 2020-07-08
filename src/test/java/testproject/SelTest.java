a5eeadac25902e3355d604023d0e9476415bd408
ajamit24 <37693051+ajamit24@users.noreply.github.com>
2020-07-08 15:21:36 +0530

    Update SelTest.java

--- src/test/java/testproject/SelTest.java
+++ src/test/java/testproject/SelTest.java
@@ -24,7 +24,7 @@ public class SelTest {
 	@Test
 	public void validateAboutPage() throws Exception {
 		System.out.println("Opening Browser");
-		//Puppet agent machine IP
+		//Puppet agent machine IP::
 		String s="http://18.217.16.65:80";
 		
 		driver.get(s);
