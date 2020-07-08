632dc707f349976078f3192937ddede57ebaca19
ajamit24 <37693051+ajamit24@users.noreply.github.com>
2020-07-08 17:19:15 +0530

    Update SelTest.java

--- src/test/java/testproject/SelTest.java
+++ src/test/java/testproject/SelTest.java
@@ -8,7 +8,7 @@ import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-//File updated on 8th July
+//File updated on 8th Jul
 public class SelTest {
 	
 	WebDriver driver;
